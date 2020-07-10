af591e513c99c994caadf913617ce35ebf46ac00
derrick <derrick56007@hotmail.com>
2020-07-10 13:16:04 -0700

    comment out local test

--- test/custom_code_upload_and_build_test.dart
+++ test/custom_code_upload_and_build_test.dart
@@ -1,47 +1,47 @@
-import 'package:code_tanks/code_tanks_server_common.dart';
-import 'package:test/test.dart';
+// import 'package:code_tanks/code_tanks_server_common.dart';
+// import 'package:test/test.dart';
 
-void main() {
-  final address = '0.0.0.0';
-  final port = 9898;
+// void main() {
+//   final address = '0.0.0.0';
+//   final port = 9898;
 
-  group('Simple Test', () {
-    DummySocket socket;
+//   group('Simple Test', () {
+//     DummySocket socket;
 
-    setUp(() async {
+//     setUp(() async {
 
-      socket = DummySocket('ws://$address:$port');
-      await socket.start();
-    });
+//       socket = DummySocket('ws://$address:$port');
+//       await socket.start();
+//     });
 
-    test('First Test', () {
-      socket.on('code_upload_success', (data) async {
-        expect(socket != null, isTrue);
-      });
+//     test('First Test', () {
+//       socket.on('code_upload_success', (data) async {
+//         expect(socket != null, isTrue);
+//       });
 
-      socket.send('code_upload', {
-        'name': 'DoNothingTank',
-        'language': 'en',
-        'code_language': 'dart',
-        'code': '''
-import 'code_tanks_api.dart';
+//       socket.send('code_upload', {
+//         'name': 'DoNothingTank',
+//         'language': 'en',
+//         'code_language': 'dart',
+//         'code': '''
+// import 'code_tanks_api.dart';
 
-class Custom extends BaseTank {
-  @override
-  void onDetectRobot(DetectRobotEvent e) {
-    // TODO: implement onDetectRobot
-  }
+// class Custom extends BaseTank {
+//   @override
+//   void onDetectRobot(DetectRobotEvent e) {
+//     // TODO: implement onDetectRobot
+//   }
 
-  @override
-  void tick() {
-    // TODO: implement tick
-  }
+//   @override
+//   void tick() {
+//     // TODO: implement tick
+//   }
   
-}
+// }
 
-BaseTank createTank() => Custom();
-'''
-      });
-    });
-  });
-}
+// BaseTank createTank() => Custom();
+// '''
+//       });
+//     });
+//   });
+// }
