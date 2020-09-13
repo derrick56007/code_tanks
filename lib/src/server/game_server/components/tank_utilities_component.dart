719cb99e71f4fad6af234cea83bbded458b9c9d4
Derrick <derrick56007@hotmail.com>
2020-09-13 16:38:45 -0700

    gun rotates with tank set to default, radar rotates with gun set to default

--- lib/src/server/game_server/components/tank_utilities_component.dart
+++ lib/src/server/game_server/components/tank_utilities_component.dart
@@ -3,9 +3,9 @@ import '../../../../code_tanks_entity_component_system.dart';
 class TankUtilitiesComponent extends Component {
   num gunRotation = 0;
   num gunAngularVelocity = 0;
-  bool gunRotatesWithTank = false;
+  bool gunRotatesWithTank = true;
 
   num radarRotation = 0;
   num radarAngularVelocity = 0;
-  bool radarRotatesWithGun = false;
+  bool radarRotatesWithGun = true;
 }
